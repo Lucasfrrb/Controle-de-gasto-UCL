4f5ecdc17a0986a564eece5456119728baf52fd8
Lucasfrrb <lucaszye380839@gmail.com>
2025-03-13 19:38:34 -0300

    menu_principal.py

--- teste01.py
+++ teste01.py
@@ -1,1 +1,9 @@
-
+while True:
+    try:
+        primeira_resposta_do_menu = int(input(f"--------Menu-------- \n1 - tralala \n2 - titiriri \n3 - tralarerotralala \n4 - aiaiaiaiaaiaai \n--------------------- \n:"))
+        if primeira_resposta_do_menu > 0 and primeira_resposta_do_menu < 5:
+            break
+        else:
+            print("insira um valor de 1 até 4!! \n. \n. \n.")
+    except ValueError:
+        print("Digite um NÚMERO de 1 até 4, por favor não insira letras!!\n. \n. \n.")
